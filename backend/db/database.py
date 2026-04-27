cf81077ffbd6f4f7d397aa7b448451f0b1cb9453
compuet-pavithra <pavithra.k@compuet.com>
2026-04-27 15:33:25 +0530

    db url modified

--- backend/db/database.py
+++ backend/db/database.py
@@ -1,9 +1,16 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+from dotenv import load_dotenv()
 
-DATABASE_URL = "postgresql://postgres:postgres@localhost:5432/ai_db"
+load_dotenv()
 
-engine = create_engine(DATABASE_URL)
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+engine = create_engine(
+    DATABASE_URL, 
+    connect_args={"sslmode": "require"}
+)
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
