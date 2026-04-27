9965d91304781a174462509a99b1d4f3df6ed3b5
Pavithra-1120 <pavikk2011@gmail.com>
2026-04-27 15:39:21 +0530

    db modified

--- backend/db/database.py
+++ backend/db/database.py
@@ -7,10 +7,8 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(
-    DATABASE_URL, 
-    connect_args={"sslmode": "require"}
-)
+engine = create_engine( DATABASE_URL, connect_args={"sslmode": "require"})
+
 SessionLocal = sessionmaker(bind=engine)
 
 Base = declarative_base()
