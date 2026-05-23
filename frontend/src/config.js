16301b2bbcf4daf54c3ff68489e3509e65623753
Pavithra K <pavikk2011@gmail.com>
2026-05-23 18:15:50 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-  BASE_URL: "http://127.0.0.1:8000",
+  // BASE_URL: "http://127.0.0.1:8000",
+  BASE_URL: "https://pavithra-1120-rag-chatbot-api.hf.space"
 };
 
 export default config;
